8cb1869201f0f14238da667770f4acbf1efcf565
RianneStreef <riannestreef@gmail.com>
2021-09-19 10:17:43 +0200

    sticky navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import Burger from "./nav/Burger";
 import banner from "../images/banner.jpg";
-import Navbar from "../components/nav/Navbar";
 
 import "../styles/Header.css";
 
+const Nav = styled.nav`
+  height: 55px;
+  width: 100%;
+  padding: 0 20px;
+  display: flex;
+  justify-content: center;
+`;
+
 const Header = () => {
+  useEffect(() => {
+    document.addEventListener("scroll", function () {
+      const nav = document.getElementById("navbar");
+      let rect = nav.getBoundingClientRect();
+      console.log("rect");
+      console.log(rect);
+      if (rect.top <= 0) {
+        // nav.classList.add("fixed_navbar");
+        console.log("fixed");
+      } else {
+        console.log("not fixed");
+
+        window.onscroll = function () {
+          myFunction();
+        };
+        function myFunction() {
+          let banner = document.getElementById("banner");
+          let bannerHeight = banner.clientHeight;
+          console.log("bannerHeight");
+          console.log(bannerHeight);
+          if (document.body.scrollTop < bannerHeight) {
+            nav.classList.remove("fixed_navbar");
+          }
+        }
+      }
+    });
+  });
+
   return (
     <div className="header">
-      <img src={banner} alt="Zebra Logo" className="banner" />
-      <Navbar />
+      <img src={banner} alt="Zebra Logo" className="banner" id="banner" />
+      <Nav id="navbar">
+        <Burger />
+      </Nav>
     </div>
   );
 };
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import Burger from "./Burger";
 
@@ -8,12 +8,31 @@ const Nav = styled.nav`
   padding: 0 20px;
   display: flex;
   justify-content: center;
-  position: fixed;
 `;
 
 const Navbar = () => {
+  useEffect(() => {
+    document.addEventListener("scroll", function () {
+      const nav = document.getElementById("navbar");
+      let rect = nav.getBoundingClientRect();
+      if (rect.top <= 0) {
+        nav.classList.add("fixed_navbar");
+      } else {
+        window.onscroll = function () {
+          myFunction();
+        };
+
+        function myFunction() {
+          if (document.body.scrollTop < 300) {
+            nav.classList.remove("fixed_navbar");
+          }
+        }
+      }
+    });
+  });
+
   return (
-    <Nav>
+    <Nav id="navbar">
       <Burger />
     </Nav>
   );
