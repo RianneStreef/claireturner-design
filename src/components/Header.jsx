6ae334ab88e8b5da4a7572676b74962edacff8fb
RianneStreef <riannestreef@gmail.com>
2021-09-19 16:12:02 +0200

    product selectors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,18 @@
-import React, { useEffect } from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from "react";
 import Burger from "./nav/Burger";
 import banner from "../images/banner.jpg";
 
 import "../styles/Header.css";
 
-const Nav = styled.nav`
-  height: 55px;
-  width: 100%;
-  padding: 0 20px;
-  display: flex;
-  justify-content: center;
-`;
-
 const Header = () => {
+  const [isHeaderSticky, setIsHeaderSticky] = React.useState(false);
+
   useEffect(() => {
     document.addEventListener("scroll", function () {
       const nav = document.getElementById("navbar");
       let rect = nav.getBoundingClientRect();
-      console.log("rect");
-      console.log(rect);
       if (rect.top <= 0) {
-        nav.classList.add("fixed_navbar");
+        // setIsHeaderSticky(true);
         console.log("fixed");
       } else {
         console.log("not fixed");
@@ -32,10 +23,8 @@ const Header = () => {
         function myFunction() {
           let banner = document.getElementById("banner");
           let bannerHeight = banner.clientHeight;
-          console.log("bannerHeight");
-          console.log(bannerHeight);
           if (document.body.scrollTop < bannerHeight) {
-            nav.classList.remove("fixed_navbar");
+            setIsHeaderSticky(false);
           }
         }
       }
@@ -45,9 +34,12 @@ const Header = () => {
   return (
     <div className="header">
       <img src={banner} alt="Zebra Logo" className="banner" id="banner" />
-      <Nav id="navbar">
+      <nav
+        id="navbar"
+        className={`nav ${isHeaderSticky ? " fixed-navbar" : ""}`}
+      >
         <Burger />
-      </Nav>
+      </nav>
     </div>
   );
 };
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,28 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
 import { graphql } from "gatsby";
 
 import "../styles/ProductPage.css";
 
 const ProductsPage = (props) => {
-  let products = props.data.allContentfulProduct.nodes;
+  const { data } = props;
+  const allProducts = data.allContentfulProduct.nodes;
+
+  const emptyQuery = "";
+
+  const [state, setState] = useState({
+    filteredData: [],
+    query: emptyQuery,
+  });
+
+  const handleInputChange = (event) => {
+    const query = event.target.value;
+    const { data } = props;
+
+    const products = data.allContentfulProduct.nodes || [];
+
+    const filteredData = products.filter((product) => {
+      const { productCategory } = product;
+      return productCategory === query;
+    });
+    setState({
+      query,
+      filteredData,
+    });
+  };
+
+  const { filteredData, query } = state;
+  const hasSearchResults = filteredData && query !== emptyQuery;
+  const products = hasSearchResults ? filteredData : allProducts;
 
   const productList = products.map((product) => {
     return (
-      <div key={product.id} className="product-card">
-        <h3 className="product-name">{product.productName}</h3>
-        <p className="product-description">{product.productDescription}</p>
-        <img
-          src={product.productImage.file.url}
-          alt={product.productName}
-          className="product-img"
-        />
+      <div
+        key={product.id}
+        className="product-card"
+        style={{
+          backgroundImage: `url(${product.productImage.file.url})`,
+          backgroundSize: "contain",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
         <p className="product-price">{product.productPrice}</p>
+        {/* <h3 className="product-name">{product.productName}</h3> */}
+        <p className="hide product-description">{product.productDescription}</p>
       </div>
     );
   });
 
-  return <div className="product-page">{productList}</div>;
+  return (
+    <>
+      <div className="product-category-selection">
+        <button
+          className="category-select-button"
+          onClick={handleInputChange}
+          value="watercolors"
+        >
+          Watercolors
+        </button>
+        <button
+          className="category-select-button"
+          onClick={handleInputChange}
+          value="digital prints"
+        >
+          Digital Prints
+        </button>
+        <button
+          className="category-select-button"
+          onClick={handleInputChange}
+          value="ink drawings"
+        >
+          Ink Drawing
+        </button>
+        <button
+          className="category-select-button"
+          onClick={handleInputChange}
+          value="acrylic"
+        >
+          Acrylic
+        </button>
+        <button
+          className="category-select-button"
+          onClick={handleInputChange}
+          value="craft kits"
+        >
+          Craft Kits
+        </button>
+        <button
+          className="category-select-button"
+          onClick={handleInputChange}
+          value="bunting"
+        >
+          Bunting
+        </button>
+        <button
+          className="category-select-button"
+          onClick={handleInputChange}
+          value=""
+        >
+          All
+        </button>
+      </div>
+      <div className="product-page">{productList}</div>
+    </>
+  );
 };
 export const productQuery = graphql`
   query productQuery {
@@ -36,6 +123,7 @@ export const productQuery = graphql`
         }
         productName
         productPrice
+        productCategory
         id
       }
     }
